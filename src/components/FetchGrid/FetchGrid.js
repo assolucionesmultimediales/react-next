7ca3316d426f9e15617b55061dd949dbe78d5679
assolucionesmultimediales <multimediales.solutions@gmail.com>
2024-05-02 22:10:34 -0300

    cambio estilos del fetchgrid

--- src/components/FetchGrid/FetchGrid.js
+++ src/components/FetchGrid/FetchGrid.js
@@ -1,18 +1,25 @@
 'use client'
 import { useEffect } from "react";
 import axios from "axios";
+import styles from './FetchGride.module.css'
 
 const FetchGrid = ()  => {
     useEffect(() =>{
         const HandleFetchData = async () => {
-            const response = await axios.get('https://ws.audioscrobbler.com/2.0/?method=artist.gettopalbums&artist=The+Strokes&api_key=d659cc02b91f8569a8d3a3e95091db43&format=json')
-            const data = response.data.topalbums.album;
-            console.log(data)
+            try{
+                const response = await axios.get('https://ws.audioscrobbler.com/2.0/?method=artist.gettopalbums&artist=The+Strokes&api_key=d659cc02b91f8569a8d3a3e95091db43&format=json')
+                const data = response.data.topalbums.album;
+                console.log(data)
+            }
+            catch(error){
+                console.log(error)
+            }
+           
         }
         HandleFetchData();
     },[])
     return(
-        <div>
+        <div className={styles.containerfetch}>
             <p>Fetch Data</p>
         </div>
     );
